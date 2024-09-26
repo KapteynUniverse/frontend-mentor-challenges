4589340264e861292dc155a3372f4a69f68338d7
KapteynUniverse <145362845+KapteynUniverse@users.noreply.github.com>
2024-09-26 21:25:12 +0300

    Add files via upload

---
+++ article-preview-component-master/index.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("share-btn1");
+const buttonTwo = document.getElementById("share-btn2");
+const profile = document.querySelector(".furniture__profile-section");
+const socials = document.querySelector(".socials");
+var w = window.innerWidth;
+
+window.onresize = window.onload = function () {
+  w = this.innerWidth;
+  socials.classList.add("hidden");
+  profile.classList.remove("hidden");
+};
+
+button.addEventListener("click", toggle);
+buttonTwo.addEventListener("click", toggle);
+
+function toggle() {
+  if (w <= 1039) {
+    profile.classList.toggle("hidden");
+    socials.classList.toggle("hidden");
+  } else {
+    socials.classList.toggle("hidden");
+    buttonTwo.classList.toggle("hidden");
+  }
+}
