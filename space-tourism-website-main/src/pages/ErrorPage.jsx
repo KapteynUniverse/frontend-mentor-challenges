c76854663fb328994fb7fb3696f53c66a29a0fbc
Asilcan Toper <145362845+KapteynUniverse@users.noreply.github.com>
2024-11-16 00:12:44 +0300

    Update ErrorPage.jsx

--- space-tourism-website-main/src/pages/ErrorPage.jsx
+++ space-tourism-website-main/src/pages/ErrorPage.jsx
@@ -4,7 +4,7 @@ function ErrorPage() {
   return (
     <main className="bg-404-page bg-cover bg-center bg-no-repeat w-full min-h-screen flex justify-center items-center">
       <Link to="/">
-        <h1 className="uppercase text-preset9 bg-blue-black text-white w-40 h-40 rounded-full flex items-center justify-center md:w-68 md:h-68 explore font-Bellefair">
+        <h1 className="uppercase text-preset9 text-center bg-blue-black text-white w-40 h-40 rounded-full flex items-center justify-center md:w-68 md:h-68 explore font-Bellefair">
           The page you are looking for has crossed the event horizon
         </h1>
       </Link>
