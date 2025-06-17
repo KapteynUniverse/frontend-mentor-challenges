0e94dd3f76bb86f93ba7b18d8dfc4663765938e7
Asilcan Toper <145362845+KapteynUniverse@users.noreply.github.com>
2025-06-17 18:43:56 +0300

    Update Form.tsx

--- multi-step-form-main/components/Form.tsx
+++ multi-step-form-main/components/Form.tsx
@@ -65,6 +65,7 @@ const Form = () => {
 
   return (
     <>
+      <h1 class="sr-only">Multi Step Form</h1>
       {step < 5 ? (
         <form ref={formRef} method="post" aria-labelledby="form-title">
           <SideStepButtons
